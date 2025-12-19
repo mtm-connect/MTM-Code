b4224d444f802cc50cf8b76fe7a454df537a51de
Matthew Wenlock <matthewwenlock@Matthews-MacBook-Pro.local>
2025-12-19 12:03:41 +0000

    Fix dark input focus styling

--- resources/views/components/text-input-dark.blade.php
+++ resources/views/components/text-input-dark.blade.php
@@ -1,3 +1,22 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'p-4 border-gray-300 bg-gray-300 bg-opacity-10 focus:font-bold  focus:bg-opacity-10 focus:border-gray-300 rounded-lg focus:ring-gray-300 ']) }}>
+<input
+    @disabled($disabled)
+    {{ $attributes->merge([
+        'class' => '
+            block w-full p-4 rounded-lg
+
+            appearance-none
+            bg-gray-300 bg-opacity-10
+            text-white placeholder-white/50
+
+            border border-gray-300
+
+            focus:outline-none
+            focus:ring-0
+            focus:ring-offset-0
+            focus:border-gray-300
+        '
+    ]) }}
+>
+
