79aa773749824c49b94d44e492f67379185a8e8d
Matthew Wenlock <matthewwenlock@Matthews-MacBook-Pro.local>
2025-12-19 13:26:16 +0000

    Fix input borders on staging by adding border class

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,10 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'p-4 border-gray-300 bg-gray-300 bg-opacity-10 focus:font-bold focus:bg-emerald-950 focus:bg-opacity-10 focus:border-emerald-950 rounded-lg focus:ring-emerald-950 ']) }}>
+<input
+    @disabled($disabled)
+    {{ $attributes->merge([
+        'class' => 'p-4 border border-gray-300 bg-gray-300 bg-opacity-10
+                   focus:font-bold focus:bg-emerald-950 focus:bg-opacity-10
+                   focus:border-emerald-950 rounded-lg focus:ring-emerald-950'
+    ]) }}
+>
